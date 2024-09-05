49f814989cba5424652db18c7f7c9d4f2aeb0bd0
oriana-monaldi <orimonaldi@gmail.com>
2024-09-05 18:33:04 -0300

    componentes

--- src/components/SobreNosotros.jsx
+++ src/components/SobreNosotros.jsx
@@ -9,7 +9,7 @@ function SobreNosotros() {
     return (
         <div className='bg-neutral-200 flex flex-col items-center justify-center'>
             <h1 className='text-3xl text-center'><strong>Sobre Nosotros</strong></h1>
-            <h2 className='text-2xl text-center mt-20'><strong>Nuestra Misión</strong></h2>
+            <h2 className='text-2xl text-center mt-20'><strong>Nuestro Impacto</strong></h2>
                 <div className='flex items-center justify-center text-center w-1/2 mt-6'>
                     <p className=''>Nuestra misión es proporcionar servicios de ambulancia rápidos, confiables y compasivos a nuestra comunidad.Estamos comprometidos con la excelencia en la atención de emergencia y el transporte médico, asegurando que cada paciente reciba el mejor cuidado posible en su momento de necesidad.</p>
                 </div>
@@ -18,7 +18,9 @@ function SobreNosotros() {
             <Card title='Ambulancias' description='Más de 20 ambulancias ' icon= {<AmbulanciaIcon/>}  ></Card>
             <Card title='Solicitudes atendidas' description='50,000 +' icon= {<SolicitudIcon/>} ></Card>
             <Card title='Pacientes atendidos' description='40,000 +' icon= {<AtencionIcon/>} ></Card>
-
+            <div>
+                <h2><strong>Nuestro Equipo</strong></h2>
+            </div>
         </div>
 )
 }
