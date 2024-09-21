4ee4052cbfb97b03c22ff2a6a87e485b2bbae23b
oriana-monaldi <orimonaldi@gmail.com>
2024-09-20 21:31:08 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { LuPhone } from "react-icons/lu";
+
 
 function Footer() {
     return (
@@ -18,19 +20,7 @@ function Footer() {
                     <strong>Contacto de emergencias</strong>
                 </h3>
                 <div className="flex items-center">
-                    <svg
-                        className="h-6 w-6 text-black"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="#2c3e50"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                    </svg>
+                    <LuPhone size={20}/>
                     <p className="m-0 ml-2">911</p>
                 </div>
             </div>
