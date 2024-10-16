4742a6e3d6759597ae10aae086934134a560cb24
oriana-monaldi <orimonaldi@gmail.com>
2024-10-15 23:44:42 -0300

    preentrega

--- src/components/Administrador/NavAdmi.jsx
+++ src/components/Administrador/NavAdmi.jsx
@@ -6,10 +6,10 @@ function NavAdmi() {
     return (
         <div className="mt-2 flex justify-between items-center px-10">
             <div className='flex justify-center w-full'>
-                <Link className="p-4 m-6 bg-red-500 text-white rounded-md" to="/tabla/ambulancia">Ambulancias</Link>
-                <Link className="p-4 m-6 bg-red-500 text-white rounded-md" to="/tabla/reporte">Reportes</Link>
-                <Link className="p-4 m-6 bg-red-500 text-white rounded-md"to="/tabla/chofer">Choferes</Link>
-                <Link className="p-4 m-6 bg-red-500 text-white rounded-md" to="/tabla/paramedico">Paramédicos</Link>
+                <Link className="p-4 m-10 bg-red-500 text-white rounded-md" to="/tabla/ambulancia">Ambulancias</Link>
+                <Link className="p-4 m-10 bg-red-500 text-white rounded-md" to="/tabla/reporte">Reportes</Link>
+                <Link className="p-4 m-10 bg-red-500 text-white rounded-md"to="/tabla/chofer">Choferes</Link>
+                <Link className="p-4 m-10 bg-red-500 text-white rounded-md" to="/tabla/paramedico">Paramédicos</Link>
             </div>
 
             <div className="ml-auto">
