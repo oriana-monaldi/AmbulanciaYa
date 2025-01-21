60dd38ff292b00215783383a1eab0e9768fbae3f
oriana-monaldi <orimonaldi@gmail.com>
2025-01-21 20:39:39 -0300

    msc

--- src/components/Administrador/Alta.jsx
+++ src/components/Administrador/Alta.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import Boton from '../Boton';
 
-// Configuración de endpoints correctos
 const API_ENDPOINTS = {
     ambulancia: '/ambulancias',
-    chofer: '/choferes', // Cambiado de chofers a choferes
+    chofer: '/choferes',
     paramedico: '/paramedicos',
     accidente: '/accidentes',
     hospital: '/hospitales',
@@ -16,10 +14,12 @@ const API_ENDPOINTS = {
 const INITIAL_STATES = {
     ambulancia: {
         patente: '',
+        inventario: false,
         vtv: false,
         seguro: false,
-        inventario: false,
-        estaEnBase: false
+        base: false,
+        choferId: '',
+        paramedicoId: '' 
     },
     chofer: {
         nombreCompleto: '',
@@ -29,7 +29,7 @@ const INITIAL_STATES = {
         nombreCompleto: '',
         dni: '',
         email: '',
-        isAdmin: false  //ESTO ES UN ERROR, HAY Q SACARLO DE LA BBDD
+        isAdmin: false
     },
     accidente: {
         direccion: '',
@@ -53,7 +53,59 @@ const INITIAL_STATES = {
 
 function Alta({ tipo }) {
     const [formData, setFormData] = useState(INITIAL_STATES[tipo] || {});
+    const [choferes, setChoferes] = useState([]);
+    const [paramedicos, setParamedicos] = useState([]);
+    const [ambulancias, setAmbulancias] = useState([]);
+    const [hospitales, setHospitales] = useState([]);
+    const [pacientes, setPacientes] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (tipo === 'ambulancia') {
+            fetchChoferes();
+            fetchParamedicos();
+        }
+        if (tipo === 'accidente') {
+            fetchAmbulancias();
+            fetchHospitales();
+        }
+    }, [tipo]);
+
+    const fetchChoferes = async () => {
+        try {
+            const response = await fetch('https://ambulanciaya.onrender.com/choferes');
+            if (!response.ok) {
+                throw new Error('Error fetching drivers');
+            }
+            const data = await response.json();
+            setChoferes(data);
+        } catch (error) {
+            console.error('Error fetching drivers:', error);
+            Swal.fire({
+                title: 'Error',
+                text: 'No se pudieron cargar los choferes',
+                icon: 'error'
+            });
+        }
+    };
+
+    const fetchParamedicos = async () => {
+        try {
+            const response = await fetch('https://ambulanciaya.onrender.com/paramedicos');
+            if (!response.ok) {
+                throw new Error('Error fetching paramedics');
+            }
+            const data = await response.json();
+            setParamedicos(data);
+        } catch (error) {
+            console.error('Error fetching paramedics:', error);
+            Swal.fire({
+                title: 'Error',
+                text: 'No se pudieron cargar los paramédicos',
+                icon: 'error'
+            });
+        }
+    };
 
     const handleInputChange = (e) => {
         const { name, value, type } = e.target;
@@ -71,10 +123,7 @@ function Alta({ tipo }) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('Datos a enviar:', JSON.stringify(formData, null, 2));
-
-        console.log('Enviando datos:', formData);
-        console.log('Endpoint:', `https://ambulanciaya.onrender.com${API_ENDPOINTS[tipo]}`);
+        console.log('Datos que estoy mandando:', JSON.stringify(formData, null, 2));
 
         try {
             const response = await fetch(`https://ambulanciaya.onrender.com${API_ENDPOINTS[tipo]}`, {
@@ -92,7 +141,6 @@ function Alta({ tipo }) {
                 throw new Error(`HTTP error! status: ${response.status}. Details: ${errorText}`);
             }
 
-            // Intentar parsear la respuesta solo si hay contenido
             const contentType = response.headers.get("content-type");
             let data;
             if (contentType && contentType.includes("application/json")) {
@@ -117,8 +165,54 @@ function Alta({ tipo }) {
             });
         }
     };
+    //Ambulancia
 
-    // El resto del código permanece igual...
+    // Add this to your existing useEffect
+    useEffect(() => {
+        if (tipo === 'ambulancia') {
+            fetchChoferes();
+            fetchParamedicos();
+        }
+        if (tipo === 'accidente') {
+            fetchAmbulancias();
+        }
+    }, [tipo]);
+
+    // Add this new fetch function
+    const fetchAmbulancias = async () => {
+        try {
+            const response = await fetch('https://ambulanciaya.onrender.com/ambulancias');
+            if (!response.ok) {
+                throw new Error('Error fetching ambulances');
+            }
+            const data = await response.json();
+            setAmbulancias(data);
+        } catch (error) {
+            console.error('Error fetching ambulances:', error);
+            Swal.fire({
+                title: 'Error',
+                text: 'No se pudieron cargar las ambulancias',
+                icon: 'error'
+            });
+        }
+    };
+
+    //Hospital
+    const fetchHospitales = async () => {
+        try {
+            const response = await fetch('https://ambulanciaya.onrender.com/hospitales');
+            if (!response.ok) throw new Error('Error fetching hospitals');
+            const data = await response.json();
+            setHospitales(data);
+        } catch (error) {
+            console.error('Error:', error);
+            Swal.fire({
+                title: 'Error',
+                text: 'No se pudieron cargar los hospitales',
+                icon: 'error'
+            });
+        }
+    };
     const renderField = (label, name, type = 'text', options = null, inputProps = {}) => (
         <div className="mb-4">
             <label className="mb-1 block font-medium text-gray-700">{label}</label>
@@ -159,22 +253,63 @@ function Alta({ tipo }) {
                 {tipo === 'ambulancia' && (
                     <>
                         {renderField('Patente', 'patente')}
-                        {renderField('VTV', 'vtv', 'select', [
-                            {value: 'true', label: 'Al día'},
-                            {value: 'false', label: 'Vencida'}
-                        ])}
-                        {renderField('Seguro', 'seguro', 'select', [
-                            {value: 'true', label: 'Al día'},
-                            {value: 'false', label: 'Vencido'}
-                        ])}
+
                         {renderField('Inventario', 'inventario', 'select', [
                             {value: 'true', label: 'Completo'},
                             {value: 'false', label: 'Incompleto'}
                         ])}
-                        {renderField('En Base', 'estaEnBase', 'select', [
-                            {value: 'true', label: 'Sí'},
+
+                        {renderField('VTV', 'vtv', 'select', [
+                            {value: 'true', label: 'Al día'},
+                            {value: 'false', label: 'Vencida'}
+                        ])}
+
+                        {renderField('Seguro', 'seguro', 'select', [
+                            {value: 'true', label: 'Al día'},
+                            {value: 'false', label: 'Vencido'}
+                        ])}
+
+                        {renderField('En Base', 'base', 'select', [
+                            {value: 'true', label: 'Si'},
                             {value: 'false', label: 'No'}
                         ])}
+                        
+            {/* ARREGLAR ESTO*/}
+                        <div className="mb-4">
+                            <label className="mb-1 block font-medium text-gray-700">Chofer</label>
+                            <select
+                                name="choferId"
+                                value={formData.choferId}
+                                onChange={handleInputChange}
+                                className="w-full rounded-md border border-red-600 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500"
+                                required
+                            >
+                                <option value="">Seleccione un chofer</option>
+                                {choferes.map(chofer => (
+                                    <option key={chofer.id} value={chofer.id}>
+                                        {chofer.nombreCompleto}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <div className="mb-4">
+                            <label className="mb-1 block font-medium text-gray-700">Paramédico</label>
+                            <select
+                                name="paramedicoId"
+                                value={formData.paramedicoId}
+                                onChange={handleInputChange}
+                                className="w-full rounded-md border border-red-600 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500"
+                                required
+                            >
+                                <option value="">Seleccione un paramédico</option>
+                                {paramedicos.map(paramedico => (
+                                    <option key={paramedico.id} value={paramedico.id}>
+                                        {paramedico.nombreCompleto}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                     </>
                 )}
 
@@ -187,9 +322,9 @@ function Alta({ tipo }) {
                 )}
                 {tipo === 'paramedico' && (
                     <>
-                        {renderField('Nombre Completo', 'nombreCompleto', 'text', null, { maxLength: 20 })}
-                        {renderField('DNI', 'dni', 'text', null, { maxLength: 20 })}
-                        {renderField('Email', 'email', 'email', null, { maxLength: 20 })}
+                        {renderField('Nombre Completo', 'nombreCompleto', 'text')}
+                        {renderField('DNI', 'dni', 'text', null)}
+                        {renderField('Email', 'email', 'email', null)}
                     </>
                 )}
 
@@ -214,6 +349,41 @@ function Alta({ tipo }) {
                         {renderField('Descripción', 'descripcion')}
                         {renderField('Fecha', 'fecha', 'date')}
                         {renderField('Hora', 'hora', 'time')}
+                        
+                        <div className="mb-4">
+                            <label className="mb-1 block font-medium text-gray-700">Ambulancia</label>
+                            <select
+                                name="ambulanciaId"
+                                value={formData.ambulanciaId}
+                                onChange={handleInputChange}
+                                className="w-full rounded-md border border-red-600 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500"
+                                required
+                            >
+                                <option value="">Seleccione una ambulancia</option>
+                                {ambulancias.map(ambulancia => (
+                                    <option key={ambulancia._id} value={ambulancia._id}>
+                                        {ambulancia.patente}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="mb-4">
+                            <label className="mb-1 block font-medium text-gray-700">Hospital</label>
+                            <select
+                                name="hospitalId"
+                                value={formData.hospitalId}
+                                onChange={handleInputChange}
+                                className="w-full rounded-md border border-red-600 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500"
+                                required
+                            >
+                                <option value="">Seleccione un hospital</option>
+                                {hospitales.map(hospital => (
+                                    <option key={hospital._id} value={hospital._id}>
+                                        {hospital.nombre}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                     </>
                 )}
 
