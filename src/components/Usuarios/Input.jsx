6679808412bd011b23f7f0052ce2752cf7de28ab
oriana-monaldi <orimonaldi@gmail.com>
2025-02-09 18:38:23 -0300

    arreglo final navbar mobile

--- src/components/Usuarios/Input.jsx
+++ src/components/Usuarios/Input.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Input = ({placeholder, onSearchChange}) => {
     return (
         <StyledWrapper>
-            <div className="group -z-10">
+            <div className="group">
                 <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
                     <g>
                         <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
@@ -20,17 +20,17 @@ const Input = ({placeholder, onSearchChange}) => {
         </StyledWrapper>
     );
 };
-
 const StyledWrapper = styled.div`
     .group {
         display: flex;
         line-height: 28px;
         align-items: center;
         position: relative;
+        max-width: 600px;
     }
 
     .input {
-        width: 400px;
+        width: 100%;
         height: 40px;
         line-height: 28px;
         padding: 0 1rem;
@@ -61,6 +61,18 @@ const StyledWrapper = styled.div`
         fill: #9e9ea7;
         width: 1rem;
         height: 1rem;
+    }
+
+    @media (max-width: 768px) {
+        .input {
+            font-size: 14px;
+        }
+    }
+
+    @media (max-width: 480px) {
+        .input {
+            font-size: 13px;
+        }
     }
 `;
 
